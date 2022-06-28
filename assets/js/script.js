8974389e4e3f44d1a28abfe1ee27c31525c821fe
Appoline Cogan <appolinecogan@gmail.com>
2022-06-28 16:20:04 +0900

    updated folder structure

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// function start quizz
+
+
+start = document.getElementById("#start-quiz");
+start.addEventListener("click", function () {
+  displayQuestion();
+  startTimer();
+});
+
+function displayQuestion() {
+
+}
+// function display a question
+
+function startTimer() {}
+// function that handles the timer
+
+// function that handles question click
+
+//function that handles quizz end
